6b09afbdf9c63db48a8117403940b37ae56f3a23
fhluo <20843637+fhluo@users.noreply.github.com>
2022-08-28 18:06:11 +0800

    docs: add usages to flags

--- cmd/root.go
+++ cmd/root.go
@@ -46,14 +46,14 @@ func init() {
 		log.Fatalln(err)
 	}
 
-	rootCmd.Flags().BoolVar(&direct, "direct", settings.Direct(), "")
-	rootCmd.Flags().BoolVar(&useProxy, "use-proxy", settings.UseProxy(), "")
-	rootCmd.Flags().BoolVar(&useScript, "use-script", settings.UseScript(), "")
-	rootCmd.Flags().BoolVar(&autoDetect, "auto-detect", settings.AutoDetect(), "")
+	rootCmd.Flags().BoolVar(&direct, "direct", settings.Direct(), "direct to net")
+	rootCmd.Flags().BoolVar(&useProxy, "use-proxy", settings.UseProxy(), "use a proxy server")
+	rootCmd.Flags().BoolVar(&useScript, "use-script", settings.UseScript(), "use setup script")
+	rootCmd.Flags().BoolVar(&autoDetect, "auto-detect", settings.AutoDetect(), "automatically detect settings")
 
-	rootCmd.Flags().StringVar(&settings.ProxyAddress, "proxy-address", settings.ProxyAddress, "")
-	rootCmd.Flags().StringVar(&settings.BypassList, "bypass-list", settings.BypassList, "")
-	rootCmd.Flags().StringVar(&settings.ScriptAddress, "script-address", settings.ScriptAddress, "")
+	rootCmd.Flags().StringVar(&settings.ProxyAddress, "proxy-address", settings.ProxyAddress, "proxy address")
+	rootCmd.Flags().StringVar(&settings.BypassList, "bypass-list", settings.BypassList, "bypass list")
+	rootCmd.Flags().StringVar(&settings.ScriptAddress, "script-address", settings.ScriptAddress, "script address")
 }
 
 func flagsChanged(cmd *cobra.Command, names ...string) bool {
