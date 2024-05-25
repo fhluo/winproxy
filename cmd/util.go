92d7643a5acbf3be2c22b4326cdad26b958f5bdf
fhluo <20843637+fhluo@users.noreply.github.com>
2024-05-25 17:35:38 +0800

    feat: use lipgloss

--- cmd/util.go
+++ cmd/util.go
@@ -1,49 +1,99 @@
 package cmd
 
 import (
-	"bufio"
 	"fmt"
-	"github.com/fatih/color"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
 	"github.com/fhluo/winproxy"
 	"github.com/fhluo/winproxy/cmd/i18n"
-	"log/slog"
-	"os"
-	"text/template"
+	"strings"
 )
 
-func PrintSettings(s winproxy.Settings) {
-	w := bufio.NewWriter(os.Stdout)
+type Settings struct {
+	Proxy         string // bool
+	ProxyAddress  string
+	Script        string // bool
+	ScriptAddress string
+	AutoDetect    string // bool
+	BypassList    []string
+}
+
+func (s Settings) BaseInfoTable() *table.Table {
 	p := i18n.GetPrinter()
-	templateStr := fmt.Sprintf(`{{red "%s"}}: {{blue "%%v" .Proxy}}
-{{red "%s"}}: {{yellow .ProxyAddress}}
-{{red "%s"}}: {{blue "%%v" .Script}}
-{{red "%s"}}: {{yellow .ScriptAddress}}
-{{red "%s"}}: {{blue "%%v" .AutoDetect}}
-{{red "%s"}}:
-  {{range .BypassList -}}
-    - {{yellow .}}
-  {{end -}}
-`,
-		p.Sprintf("Use proxy"), p.Sprintf("Proxy address"),
-		p.Sprintf("Use script"), p.Sprintf("Script address"),
-		p.Sprintf("Auto-detect"), p.Sprintf("Bypass list"),
-	)
 
-	settingsTemplate := template.Must(template.New("").Funcs(
-		template.FuncMap{
-			"red":    color.RedString,
-			"blue":   color.BlueString,
-			"yellow": color.YellowString,
-		},
-	).Parse(templateStr))
+	t := table.New()
 
-	if err := settingsTemplate.Execute(w, s); err != nil {
-		slog.Error("failed to execute settings template", "err", err)
-		os.Exit(1)
+	t.Border(lipgloss.NormalBorder())
+	t.BorderStyle(lipgloss.NewStyle().Foreground(lipgloss.Color("#31304D")))
+
+	t.StyleFunc(func(row, col int) lipgloss.Style {
+		if col == 0 {
+			return lipgloss.NewStyle().Align(lipgloss.Center).Width(16).Foreground(lipgloss.Color("#B99470")).Bold(true)
+		} else {
+			return lipgloss.NewStyle().Align(lipgloss.Center).Width(16).Foreground(lipgloss.Color("#F0ECE5"))
+		}
+	})
+
+	t.Row(p.Sprintf("Use proxy"), s.Proxy)
+	t.Row(p.Sprintf("Proxy address"), s.ProxyAddress)
+	t.Row(p.Sprintf("Use script"), s.Script)
+	t.Row(p.Sprintf("Script address"), s.ScriptAddress)
+	t.Row(p.Sprintf("Auto-detect"), s.AutoDetect)
+
+	return t
+}
+
+func (s Settings) BypassListTable() *table.Table {
+	p := i18n.GetPrinter()
+
+	t := table.New()
+
+	t.Border(lipgloss.NormalBorder())
+	t.BorderStyle(lipgloss.NewStyle().Foreground(lipgloss.Color("#31304D")))
+
+	t.StyleFunc(func(row, col int) lipgloss.Style {
+		if row == 0 {
+			return lipgloss.NewStyle().Align(lipgloss.Center).Width(32).Foreground(lipgloss.Color("#B99470")).Bold(true)
+		} else {
+			return lipgloss.NewStyle().Align(lipgloss.Center).Width(32).Foreground(lipgloss.Color("#F0ECE5"))
+		}
+	})
+
+	t.Headers(p.Sprintf("Bypass list"))
+
+	for _, address := range s.BypassList {
+		t.Row(address)
 	}
 
-	if err := w.Flush(); err != nil {
-		slog.Error("failed to flush", "err", err)
-		os.Exit(1)
+	return t
+}
+
+func (s Settings) String() string {
+	return strings.Join([]string{
+		fmt.Sprint(s.BaseInfoTable()),
+		fmt.Sprint(s.BypassListTable()),
+	}, "\n")
+}
+
+func CheckMark(b bool) string {
+	if b {
+		return "✓"
+	} else {
+		return "𐄂"
 	}
+}
+
+func Render(s winproxy.Settings) Settings {
+	return Settings{
+		Proxy:         CheckMark(s.Proxy),
+		ProxyAddress:  s.ProxyAddress,
+		Script:        CheckMark(s.Script),
+		ScriptAddress: s.ScriptAddress,
+		AutoDetect:    CheckMark(s.AutoDetect),
+		BypassList:    s.BypassList,
+	}
+}
+
+func PrintSettings(s winproxy.Settings) {
+	fmt.Println(Render(s))
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/fhluo/winproxy
 go 1.21
 
 require (
-	github.com/fatih/color v1.17.0
+	github.com/charmbracelet/lipgloss v0.11.0
 	github.com/samber/lo v1.39.0
 	github.com/spf13/cobra v1.8.0
 	golang.org/x/sys v0.20.0
@@ -11,9 +11,14 @@ require (
 )
 
 require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/x/ansi v0.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
+	golang.org/x/exp v0.0.0-20240525044651-4c93da0ed11d // indirect
 )
