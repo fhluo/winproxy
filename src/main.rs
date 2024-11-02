f0dfd3b82990b5e2067309bf5b62f5a502445a57
fhluo <20843637+fhluo@users.noreply.github.com>
2024-11-03 03:00:49 +0800

    refactor: implement all_none and write_settings methods for Args

--- src/main.rs
+++ src/main.rs
@@ -22,50 +22,62 @@ struct Args {
     bypass_list: Option<Vec<String>>,
 }
 
+impl Args {
+    fn all_none(&self) -> bool {
+        matches!(self, Args {
+            use_proxy: None,
+            use_script: None,
+            auto_detect: None,
+            proxy_address: None,
+            script_address: None,
+            bypass_list: None
+        })
+    }
+
+    fn write_settings(self, settings: &mut DefaultConnectionSettings) {
+        if let Some(use_proxy) = self.use_proxy {
+            settings.flags.set(Flags::Proxy, use_proxy)
+        }
+
+        if let Some(use_script) = self.use_script {
+            settings.flags.set(Flags::AutoProxyURL, use_script)
+        }
+
+        if let Some(auto_detect) = self.auto_detect {
+            settings.flags.set(Flags::AutoDetect, auto_detect)
+        }
+
+        if let Some(proxy_address) = self.proxy_address {
+            settings.proxy_address = proxy_address;
+        }
+
+        if let Some(script_address) = self.script_address {
+            settings.script_address = script_address;
+        }
+
+        if let Some(bypass_list) = self.bypass_list {
+            settings.bypass_list = bypass_list;
+        }
+    }
+}
+
 fn main() {
     let args = Args::parse();
-
-    if matches!(args, Args {
-        use_proxy: None,
-        use_script: None,
-        auto_detect: None,
-        proxy_address: None,
-        script_address: None,
-        bypass_list: None
-    }) {
-        return;
-    }
 
     let mut settings = DefaultConnectionSettings::from_registry()
         .expect("failed to read default connection settings from registry");
 
-    if let Some(use_proxy) = args.use_proxy {
-        settings.flags.set(Flags::Proxy, use_proxy)
+    if args.all_none() {
+        show_settings(&settings);
+    } else {
+        args.write_settings(&mut settings);
+        settings.version += 1;
+        settings
+            .write_registry()
+            .expect("failed to write default connection settings to registry");
     }
+}
 
-    if let Some(use_script) = args.use_script {
-        settings.flags.set(Flags::AutoProxyURL, use_script)
-    }
-
-    if let Some(auto_detect) = args.auto_detect {
-        settings.flags.set(Flags::AutoDetect, auto_detect)
-    }
-
-    if let Some(proxy_address) = args.proxy_address {
-        settings.proxy_address = proxy_address;
-    }
-
-    if let Some(script_address) = args.script_address {
-        settings.script_address = script_address;
-    }
-
-    if let Some(bypass_list) = args.bypass_list {
-        settings.bypass_list = bypass_list;
-    }
-
-    settings.version += 1;
-
-    settings
-        .write_registry()
-        .expect("failed to write default connection settings to registry");
+fn show_settings(settings: &DefaultConnectionSettings) {
+    println!("{:#?}", settings);
 }
