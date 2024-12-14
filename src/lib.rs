7178d7bd402a24ae7c53b5ada25568505fbe9216
fhluo <20843637+fhluo@users.noreply.github.com>
2024-12-14 21:02:32 +0800

    refactor: improve error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use bitflags::bitflags;
-use std::error::Error;
-use std::fmt;
-use std::fmt::{Debug, Formatter};
+use std::fmt::{Debug, Display, Formatter};
 use std::io::{Cursor, Read, Write};
+use std::string::FromUtf8Error;
+use std::{error, fmt, io, result};
 use windows_registry::{Value, CURRENT_USER};
 
 bitflags! {
@@ -51,28 +51,73 @@ impl Debug for DefaultConnectionSettings {
     }
 }
 
+#[derive(Debug)]
+pub enum Error {
+    IOError(io::Error),
+    FromUtf8Error(FromUtf8Error),
+    WindowsResultError(windows_result::Error),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::IOError(err) => Display::fmt(err, f),
+            Error::FromUtf8Error(err) => Display::fmt(err, f),
+            Error::WindowsResultError(err) => Display::fmt(err, f),
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match self {
+            Error::IOError(err) => Some(err),
+            Error::FromUtf8Error(err) => Some(err),
+            Error::WindowsResultError(err) => Some(err),
+        }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        Self::IOError(err)
+    }
+}
+
+impl From<FromUtf8Error> for Error {
+    fn from(err: FromUtf8Error) -> Self {
+        Self::FromUtf8Error(err)
+    }
+}
+
+impl From<windows_result::Error> for Error {
+    fn from(err: windows_result::Error) -> Self {
+        Self::WindowsResultError(err)
+    }
+}
+
+pub type Result<T> = result::Result<T, Error>;
+
 trait Reader {
-    fn read_u32(&mut self) -> Result<u32, Box<dyn Error>>;
-    fn read_string(&mut self) -> Result<String, Box<dyn Error>>;
+    fn read_u32(&mut self) -> Result<u32>;
+    fn read_string(&mut self) -> Result<String>;
 }
 
 trait Writer {
-    fn write_u32(&mut self, value: u32) -> Result<(), Box<dyn Error>>;
-    fn write_string(&mut self, value: &str) -> Result<(), Box<dyn Error>>;
+    fn write_u32(&mut self, value: u32) -> Result<()>;
+    fn write_string(&mut self, value: &str) -> Result<()>;
 }
 
 impl Reader for Cursor<&[u8]> {
-    fn read_u32(&mut self) -> Result<u32, Box<dyn Error>> {
+    fn read_u32(&mut self) -> Result<u32> {
         let mut buffer = [0u8; 4];
         self.read_exact(&mut buffer)?;
 
         Ok(u32::from_le_bytes(buffer))
     }
 
-    fn read_string(&mut self) -> Result<String, Box<dyn Error>> {
-        let length = self.read_u32()? as usize;
-
-        let mut buffer = vec![0u8; length];
+    fn read_string(&mut self) -> Result<String> {
+        let mut buffer = vec![0u8; self.read_u32()? as usize];
         self.read_exact(&mut buffer)?;
 
         Ok(String::from_utf8(buffer)?)
@@ -80,26 +125,26 @@ impl Reader for Cursor<&[u8]> {
 }
 
 impl Writer for Cursor<Vec<u8>> {
-    fn write_u32(&mut self, value: u32) -> Result<(), Box<dyn Error>> {
-        self.write_all(&value.to_le_bytes()).map_err(|e| e.into())
+    fn write_u32(&mut self, value: u32) -> Result<()> {
+        self.write_all(&value.to_le_bytes()).map_err(Into::into)
     }
 
-    fn write_string(&mut self, value: &str) -> Result<(), Box<dyn Error>> {
+    fn write_string(&mut self, value: &str) -> Result<()> {
         self.write_u32(value.len() as u32)?;
-        self.write_all(value.as_bytes()).map_err(|e| e.into())
+        self.write_all(value.as_bytes()).map_err(Into::into)
     }
 }
 
 impl TryFrom<&[u8]> for DefaultConnectionSettings {
-    type Error = Box<dyn Error>;
+    type Error = Error;
 
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(value: &[u8]) -> Result<Self> {
         let mut cursor = Cursor::new(value);
 
         Ok(Self {
             unknown: cursor.read_u32()?,
             version: cursor.read_u32()?,
-            flags: Flags::from_bits(cursor.read_u32()?).ok_or("")?,
+            flags: Flags::from_bits_retain(cursor.read_u32()?),
             proxy_address: cursor.read_string()?,
             bypass_list: {
                 cursor
@@ -119,9 +164,9 @@ impl TryFrom<&[u8]> for DefaultConnectionSettings {
 }
 
 impl TryFrom<DefaultConnectionSettings> for Vec<u8> {
-    type Error = Box<dyn Error>;
+    type Error = Error;
 
-    fn try_from(settings: DefaultConnectionSettings) -> Result<Self, Self::Error> {
+    fn try_from(settings: DefaultConnectionSettings) -> Result<Self> {
         let mut cursor = Cursor::new(Vec::<u8>::new());
 
         cursor.write_u32(settings.unknown)?;
@@ -146,30 +191,31 @@ impl TryFrom<DefaultConnectionSettings> for Vec<u8> {
 }
 
 impl DefaultConnectionSettings {
-    const KEY_PATH: &'static str = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Connections";
+    const KEY_PATH: &'static str =
+        r"SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Connections";
     const VALUE_NAME: &'static str = "DefaultConnectionSettings";
 
     #[inline]
-    fn get_registry_value() -> Result<Value, Box<dyn Error>> {
+    fn get_registry_value() -> Result<Value> {
         CURRENT_USER
             .open(Self::KEY_PATH)?
             .get_value(Self::VALUE_NAME)
-            .map_err(|err| err.into())
+            .map_err(Into::into)
     }
 
     #[inline]
-    fn set_registry_value(value: &Value) -> Result<(), Box<dyn Error>> {
+    fn set_registry_value(value: &Value) -> Result<()> {
         CURRENT_USER
             .open(Self::KEY_PATH)?
             .set_value(Self::VALUE_NAME, value)
-            .map_err(|err| err.into())
+            .map_err(Into::into)
     }
 
-    pub fn from_registry() -> Result<Self, Box<dyn Error>> {
+    pub fn from_registry() -> Result<Self> {
         Ok(Self::get_registry_value()?.as_ref().try_into()?)
     }
 
-    pub fn write_registry(self) -> Result<(), Box<dyn Error>> {
+    pub fn write_registry(self) -> Result<()> {
         Self::set_registry_value(&Value::from(&TryInto::<Vec<u8>>::try_into(self)? as &[u8]))
     }
 }
