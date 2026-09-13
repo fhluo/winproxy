1d91c3fad501ffc57910ddf694f5e52a5c796e7b
fhluo <20843637+fhluo@users.noreply.github.com>
2026-09-13 17:32:15 +0800

    refactor: drop JSON tags from `DefaultConnectionSettings`

--- go/settings/settings_test.go
+++ go/settings/settings_test.go
@@ -13,7 +13,17 @@ func TestDefaultConnectionSettings(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	data, err := json.Marshal(s, jsontext.WithIndent("  "))
+	settings := struct {
+		Unknown       int32    `json:"unknown"`
+		Version       int32    `json:"version"`
+		Flags         int32    `json:"flags"`
+		ProxyAddress  string   `json:"proxy_address"`
+		BypassList    string   `json:"bypass_list"`
+		ScriptAddress string   `json:"script_address"`
+		Unknown1      [32]byte `json:"unknown1"`
+	}(*s)
+
+	data, err := json.Marshal(settings, jsontext.WithIndent("  "))
 	if err != nil {
 		t.Fatal(err)
 	}
